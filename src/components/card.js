729291dc76e226ee7bb88b4f3a2e11e903b63dc0
Fedor Iskandarov <f.iskandarov@cl.world>
2020-07-24 03:32:18 +0300

    feat: added full size link to card, upd styles

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,56 @@
 import React from "react";
-
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+
+const Root = styled(Link)`
+  height: 100%;
+  box-shadow: 0px 4px 11px rgba(132, 140, 168, 0.1);
+  transition: 0.15s ease-out;
+  transition-property: color, box-shadow;
+
+  &:hover {
+    color: currentColor;
+    text-decoration: none;
+    box-shadow: 0px 4px 11px rgba(132, 140, 168, 0.15);
+
+    .card-img-top {
+      transform: scale(1.125);
+    }
+  }
+`;
+
+const Image = styled.div`
+  overflow: hidden;
+
+  .card-img-top {
+    transition: transform 0.3s ease-out;
+    will-change: transform;
+  }
+`;
+
+const Text = styled.div`
+  padding: 1rem;
+
+  h3 {
+    margin-bottom: 0.25rem;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+`;
 
 const Card = (state) => {
   const { user } = state;
 
   return (
-    <div className="card">
-      <img src={user.avatar_url} alt={user.login} className="card-img-top" />
-
-      <div className="card-body">
-        <h3 className="card-title">{user.login}</h3>
-        <Link to={`/profile/${user.login}`} className="btn btn-primary">
-          Открыть
-        </Link>
-      </div>
-    </div>
+    <Root to={`/profile/${user.login}`} className="card">
+      <Image>
+        <img src={user.avatar_url} alt={user.login} className="card-img-top" />
+      </Image>
+      <Text>
+        <h3>{user.login}</h3>
+      </Text>
+    </Root>
   );
 };
 
